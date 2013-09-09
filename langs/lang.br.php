5d971169d21e59f697fdd6d0872e645b1c69f12f
guilhermecaldas <guilhermecaldas@yandex.com>
2013-09-09 10:52:59 -0300

    Updated language files

--- langs/lang.br.php
+++ langs/lang.br.php
@@ -1,4 +1,4 @@
-<?php
+﻿<?php
 /* 
 -----------------
 Language: Br
@@ -13,7 +13,7 @@ $lang['MENU_ABOUT'] = 'Sobre';
 $lang['MENU_CONTACT'] = 'Contato';
 $lang['MENU_LOGREG'] = 'Login / Registrar';
 $lang['MENU_ADMIN'] = 'Painel de Administração';
-$lang['MENU_ALPHA'] = 'Alpha teste';
+$lang['MENU_ALPHA'] = 'AlphaLab';
 $lang['MENU_EXIT'] = 'Sair';
 $lang['MENU_SETTINGS'] = 'Configurações';
 $lang['MENU_LOGIN'] = 'Login';
@@ -21,26 +21,26 @@ $lang['MENU_REGISTER'] = 'Registrar';
 $lang['MENU_LANG'] = 'Idioma';
 //Downloads
 $lang['D_DOWNLOAD'] = 'Download Chemis';
-$lang['D_CAL'] = 'Chemis é constantemente atualizado';
-$lang['D_LOC'] = 'Nossos produtos são protegidos';
-$lang['D_CALK'] = 'Com Chemis, tu tens os resultados mais precisos';
-$lang['D_USERS'] = 'Nós nos preocupamos com os usuários';
+$lang['D_CAL'] = 'Atualizações constantes';
+$lang['D_LOC'] = 'Versão mobile';
+$lang['D_CALK'] = 'Resultados rápidos';
+$lang['D_USERS'] = 'Envie sua sugestão';
 //Contact
 $lang['C_TITLE'] = 'Supporte & Contato';
-$lang['C_SLOGAN'] = 'Nós estaremos sempre prontos para aceitar propostas para o projeto.<br>Entre em contato com:';
-$lang['C_GUILHERME'] = 'Guilherme Caldas - Lider de desenvolvimento, gerente de projeto';
-$lang['C_NIKITA'] = 'Nikita Berezhnoj - Desenvolvedor web, designer';
-$lang['C_ANA'] = 'Ana Evangelho - Gerencia de conteúdo, revisora';
+$lang['C_SLOGAN'] = 'Envie sugestões para o projeto.<br>Entre em contato com a equipe:';
+$lang['C_GUILHERME'] = 'Guilherme Caldas - Líder de desenvolvimento, gerente de projeto';
+$lang['C_NIKITA'] = 'Nikita Berezhnoj - Desenvolvedor web, web designer';
+$lang['C_ANA'] = 'Ana Evangelho - Gerente de conteúdo, revisora';
 //Alpha
-$lang['A_TITLE'] = 'Unidade AlphaTeste';
-$lang['A_SLOGAN'] = 'ChemisProject possui uma divisão focada em testes.';
-$lang['A_SLOGAN2'] = 'Ela se chama <strong>AlphaLab</strong> e possui sua propria atmosfera e cultura.';
+$lang['A_TITLE'] = 'Unidade AlphaLab';
+$lang['A_SLOGAN'] = 'ChemisProject possui uma unidade para testes.';
+$lang['A_SLOGAN2'] = 'Ela se chama <strong>AlphaLab</strong> e opera de forma independente.';
 $lang['A_BTN'] = 'Junte-se à equipe';
-$lang['A_ALREADY'] = 'Tu já se inscreveu! Aguarde a decisão da administração.';
-$lang['A_NO'] = 'Administration is not your application and you can download the Chemis-alpha.';
-$lang['A_YES'] = 'A administração aprovou seu pedido! Tu já podes fazer o donwload de ChemisAlpha!';
+$lang['A_ALREADY'] = 'Você já está inscrito. Aguarde a decisão da equipe.';
+$lang['A_NO'] = 'Sua inscrição não foi aceita. Você pode fazer o download do Chemis Alpha.';
+$lang['A_YES'] = 'Sua inscrição foi aceita. Você pode fazer o donwload do Chemis Alpha.';
 $lang['A_DOWNLOAD'] = 'Download ChemisDesktop Alpha';
-$lang['A_WAIT'] = 'Sua requisição está sob análise da administração.';
+$lang['A_WAIT'] = 'Sua inscrição está sob análise. Aguarde.';
 //Settings
 $lang['E_TITLE'] = 'Editar perfil';
 $lang['E_FIRST'] = 'Primeiro nome';
@@ -49,11 +49,11 @@ $lang['E_PASS'] = 'Senha';
 $lang['E_REPASS'] = 'Repita a senha';
 $lang['E_MAIL'] = 'Email';
 $lang['E_POST'] = 'Salvar';
-$lang['E_FIELDS'] = 'Por favor, insira todos os campos no formulário';
-$lang['E_TRY'] = 'Tente denovo';
-$lang['E_VMAIL'] = 'Por favor, insira um email válido';
-$lang['E_MPASS'] = 'As senhas escolhidas não estão de acordo uma com a outra';
+$lang['E_FIELDS'] = 'Por favor, insira todos os campos no formulário.';
+$lang['E_TRY'] = 'Tente novamente.';
+$lang['E_VMAIL'] = 'Por favor, insira um email válido.';
+$lang['E_MPASS'] = 'Senha não confere. Digite novamente.';
 $lang['E_ERRORS'] = 'Os seguintes erros ocorreram durante o processamento:';
-$lang['E_LSPASS'] = 'As senhas devem estar de acordo uma com a outra e ter no mínimo 8 caracteres';
+$lang['E_LSPASS'] = 'As senhas não conferem e devem ter no mínimo 8 caracteres.';
 $lang['E_SE'] = 'Sua conta foi atualizada com sucesso!';
 ?>
--- langs/lang.en.php
+++ langs/lang.en.php
@@ -1,4 +1,4 @@
-<?php
+﻿<?php
 /* 
 ------------------
 Language: En
@@ -13,34 +13,34 @@ $lang['MENU_ABOUT'] = 'About';
 $lang['MENU_CONTACT'] = 'Contact';
 $lang['MENU_LOGREG'] = 'Login / Register';
 $lang['MENU_ADMIN'] = 'Admin panel';
-$lang['MENU_ALPHA'] = 'Alpha test';
+$lang['MENU_ALPHA'] = 'AlphaLab';
 $lang['MENU_EXIT'] = 'Exit';
 $lang['MENU_SETTINGS'] = 'Settings';
 $lang['MENU_LOGIN'] = 'Login';
 $lang['MENU_REGISTER'] = 'Register';
-$lang['MENU_LANG'] = 'language';
+$lang['MENU_LANG'] = 'Language';
 //Downloads
 $lang['D_DOWNLOAD'] = 'Download Chemis';
-$lang['D_CAL'] = 'Chemis is constantly updated';
-$lang['D_LOC'] = 'Our products are protected';
-$lang['D_CALK'] = 'With Chemis you have the most accurate results';
-$lang['D_USERS'] = 'We care about users';
+$lang['D_CAL'] = 'Constant updates';
+$lang['D_LOC'] = 'Mobile version';
+$lang['D_CALK'] = 'Fast results';
+$lang['D_USERS'] = 'Submit suggestions';
 //Contact
 $lang['C_TITLE'] = 'Support & Contact';
-$lang['C_SLOGAN'] = 'We will be ready to accept any proposals for the project!<br>Communication with the command:';
-$lang['C_GUILHERME'] = 'Guilherme Caldas - lead developer, project manager ';
-$lang['C_NIKITA'] = 'Nikita Berezhnoj - Web developer, designer';
+$lang['C_SLOGAN'] = 'Submit suggestions for the project.<br>Contact the team:';
+$lang['C_GUILHERME'] = 'Guilherme Caldas - Lead developer, project manager ';
+$lang['C_NIKITA'] = 'Nikita Berezhnoj - Web developer, web designer';
 $lang['C_ANA'] = 'Ana Evangelho - Content manager, reviewer ';
 //Alpha
-$lang['A_TITLE'] = 'Unit AlphaTest';
-$lang['A_SLOGAN'] = 'ChemisProject has a division focused on testing.';
-$lang['A_SLOGAN2'] = 'Its called by <strong>AlphaLab</strong> and has its own atmosphere and culture.';
+$lang['A_TITLE'] = 'AlphaLab Unit';
+$lang['A_SLOGAN'] = 'ChemisProject has an unit focused on testing.';
+$lang['A_SLOGAN2'] = 'It is called <strong>AlphaLab</strong> and it operates independently.';
 $lang['A_BTN'] = 'Join the team';
-$lang['A_ALREADY'] = 'You have already applied! Wait for the administrations decision';
-$lang['A_NO'] = 'Administration is not your application and you can download the Chemis-alpha.';
-$lang['A_YES'] = 'Administration has approved your request and you can download the Chemis-alpha.';
+$lang['A_ALREADY'] = 'You have already applied. Please wait for the team\'s decision.';
+$lang['A_NO'] = 'Your request was denied. You can download ChemisAlpha.';
+$lang['A_YES'] = 'Your request was approved. You can download ChemisAlpha.';
 $lang['A_DOWNLOAD'] = 'Download ChemisDesktop Alpha';
-$lang['A_WAIT'] = 'Your request is under consideration by the administration.';
+$lang['A_WAIT'] = 'Your request is under review. Please wait.';
 //Settings
 $lang['E_TITLE'] = 'Edit profile';
 $lang['E_FIRST'] = 'First name';
@@ -49,16 +49,16 @@ $lang['E_PASS'] = 'Password';
 $lang['E_REPASS'] = 'Retype password';
 $lang['E_MAIL'] = 'Email address';
 $lang['E_POST'] = 'Save';
-$lang['E_FIELDS'] = 'Please enter all fields in the form';
+$lang['E_FIELDS'] = 'Please enter all fields in the form.';
 $lang['E_TRY'] = 'Try again';
-$lang['E_VMAIL'] = 'Please enter a valid email address';
-$lang['E_MPASS'] = 'The passwords you entered dont match';
-$lang['E_ERRORS'] = 'The following errors have occured while processing:';
-$lang['E_LSPASS'] = 'Make certain the passwords match and are at least 8 characters in length';
-$lang['E_SE'] = 'You successfully updated account!';
+$lang['E_VMAIL'] = 'Please enter a valid email address.';
+$lang['E_MPASS'] = 'The passwords you entered don\'t match.';
+$lang['E_ERRORS'] = 'The following errors have occurred while processing:';
+$lang['E_LSPASS'] = 'The passwords must match and be at least 8 characters long.';
+$lang['E_SE'] = 'Your account has been successfully updated.';
 //Site offline
-$lang['O_TITLE'] = '<center><b>Lord! Going technical work on the server. authorization is not available.</b>
-<br>You can follow us via <a href="http://vk.com/chemisproject" target="_blank">Вконтакте</a> or
+$lang['O_TITLE'] = '<center><b>Site under repair. We are sorry for the inconvenience.</b>
+<br>You can follow us on <a href="http://vk.com/chemisproject" target="_blank">Вконтакте</a> or
 <a href="https://www.facebook.com/chemisproject" target="_blank">Facebook</a></center>';
-$lang['O_AUTH'] = '<center><b>Going technical work on the server. authorization is not available.</b><center>';
+$lang['O_AUTH'] = '<center><b>Site under repair. We are sorry for the inconvenience.</b><center>';
 ?>
