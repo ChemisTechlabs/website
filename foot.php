8ebbd540764094ba44aac213d4b3363ae23de1c4
guilhermecaldas <guilhermecaldas@yandex.com>
2013-11-18 15:08:40 -0200

    Full wipe

--- foot.php
+++
@@ -1,7 +0,0 @@
-<div class="modal-footer">
-    <div class="row-fluid">
-        &copy; 2013 Chemis Team. <a href="http://vk.com/nikitoshi2013" target="_blank">Nikita Berezhnoj</a> 
-        &middot; <a href="http://vk.com/guilherme_caldas" target="_blank">Guilherme Caldas</a> 
-        &middot; <a href="http://vk.com/anaevans" target="_blank">Ana Evangelho</a>
-    </div>
-</div>
