f84b96af5df4840cc1bc27ab1911e42fcb561d5e
guilhermecaldas <guilhermecaldas@yandex.com>
2013-11-19 15:12:34 -0200

    New layout plan

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+    <meta charset="utf-8">
+    <title>ChemisProject</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="keywords" content="Chemis, Chemis project, Калькулятор, Сalculator, Химия, chemistry">
+    <meta name="description" content="Chemis is a project of chemistry software, to helps chemists and students.">
+    <meta name="version" content="Plum">
+    <meta name="author" content="Никита Бережной">
+    <!--Css-->
+    <link href="css/pure-min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <!--javascript-->
+    <script src="js/jquery.js"></script>
+</head>
+
+<body>
+    <div class="pure-menu pure-menu-open pure-menu-horizontal chemis-menu-bar">
+        <ul>
+            <li>
+                <a href="#">Home</a>
+            </li>
+            <li class="pure-menu-selected">
+                <a href="#">About</a>
+            </li>
+            <li>
+                <a href="#">Download</a>
+            </li>
+            <li>
+                <a href="#">Contact</a>
+            </li>
+        </ul>
+    </div>
+
+    <div class="pure-g-r chemis-page">
+        <div id="session-home" class="pure-u-1">
+            oi
+        </div>
+
+        <div id="session-about" class="pure-u-1">
+            oi
+        </div>
+
+    </div>
+</body>
+
+</html>
